f269f1c829ff02c8be154c089c2da1b442bcaca2
MariMartins120 <mariane.martins.120@gmail.com>
2025-09-02 12:27:35 -0300

    Adic

--- src/ClasseAutomotores.java
+++ src/ClasseAutomotores.java
@@ -1,4 +1,56 @@
 
-public class ClasseAutomotores {
 
-}
+	public class ClasseAutomotores {
+	 
+	   String corAt;
+	   String marcaAt;
+	   String modeloAt;
+	   String tipoAt;
+
+
+	    public ClasseAutomotores() {
+	    }
+
+
+	    public ClasseAutomotores(String corPar, String marcaPar, String modeloPar, String tipoPar) {
+	        this.corAt = corPar;
+	        this.marcaAt = marcaPar;
+	        this.modeloAt = modeloPar;
+	        this.tipoAt = tipoPar;
+	    }
+
+	    
+        public String getCorAt() {
+            return corAt;
+        }
+
+        public String getMarcaAt() {
+            return marcaAt;
+        }
+
+        public String getModeloAt() {
+            return modeloAt;
+        }
+
+        public String getTipoAt() {
+            return tipoAt;
+        }
+
+        public void setCorAt(String corPar) {
+            this.corAt = corPar;
+        }
+
+        public void setMarcaAt(String marcaAt) {
+            this.marcaAt = marcaAt;
+        }
+
+        public void setModeloAt(String modeloAt) {
+            this.modeloAt = modeloAt;
+        }
+
+        public void setTipoAt(String tipoAt) {
+            this.tipoAt = tipoAt;
+        }
+	    }
+
+	  
