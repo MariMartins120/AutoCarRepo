1b35c7412bc12aa14ad73b16fbc61c8d2e5de178
MariMartins120 <mariane.martins.120@gmail.com>
2025-09-11 12:05:25 -0300

    Concluido o codigo

--- src/ClasseAutomotores.java
+++ src/ClasseAutomotores.java
@@ -1,56 +1,56 @@
 
 
-	public class ClasseAutomotores {
-	 
-	   String corAt;
-	   String marcaAt;
-	   String modeloAt;
-	   String tipoAt;
+public class ClasseAutomotores {
+	
+	String corAt;
+	String marcaAt;
+	String modeloAt;
+	String tipoAt;
+	
+    public ClasseAutomotores () {
+		
+	}
+	
+	public ClasseAutomotores (String corPar, String marcaPar, String modeloPar, String tipoPar) {
+		this.corAt = corPar;
+		this.marcaAt = marcaPar;
+		this.modeloAt = modeloPar;
+		this.tipoAt = tipoPar;
+		
+	}
+	
+	public String getcorAt() {
+		return corAt;
+	}
+	
+	public void setcorAt(String corPar) {
+		this.corAt = corPar;
+	}
+	
+	
+	public String getmarcaAt() {
+		return marcaAt;
+	}
+	
+	public void setmarcaAt(String marcaPar) {
+		this.marcaAt = marcaPar;
+	}
+	
+	public String getmodeloAt() {
+		return modeloAt;
+	}
+	
+	public void setmodeloAt(String modeloPar) {
+		this.modeloAt = modeloPar;
+	}
+	
+	
+	public String gettipoAt() {
+		return tipoAt;
+	}
+	
+	public void settipoAt(String tipoPar) {
+		this.tipoAt = tipoPar;
+	}
 
-
-	    public ClasseAutomotores() {
-	    }
-
-
-	    public ClasseAutomotores(String corPar, String marcaPar, String modeloPar, String tipoPar) {
-	        this.corAt = corPar;
-	        this.marcaAt = marcaPar;
-	        this.modeloAt = modeloPar;
-	        this.tipoAt = tipoPar;
-	    }
-
-	    
-        public String getCorAt() {
-            return corAt;
-        }
-
-        public String getMarcaAt() {
-            return marcaAt;
-        }
-
-        public String getModeloAt() {
-            return modeloAt;
-        }
-
-        public String getTipoAt() {
-            return tipoAt;
-        }
-
-        public void setCorAt(String corPar) {
-            this.corAt = corPar;
-        }
-
-        public void setMarcaAt(String marcaAt) {
-            this.marcaAt = marcaAt;
-        }
-
-        public void setModeloAt(String modeloAt) {
-            this.modeloAt = modeloAt;
-        }
-
-        public void setTipoAt(String tipoAt) {
-            this.tipoAt = tipoAt;
-        }
-	    }
-
-	  
+}
